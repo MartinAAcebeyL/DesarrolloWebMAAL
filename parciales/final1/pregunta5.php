2d93d890c1df8cb63feff2a1a210e0162c7bd415
MartinAAcebeyL <martinaacbyl2000pro@gmail.com>
2021-07-21 17:59:17 -0400

    final

--- parciales/final1/pregunta5.php
+++ parciales/final1/pregunta5.php
@@ -1,7 +1,7 @@
 <div style="display: flex; justify-content: space-between; width: 75%;">
     <a id="uno" style="border: 1px solid black;" href="javascript:cargarContenido_correos('readCorreos.php')">Bandeja de entrada</a>
     <a id="dos" style="border: 1px solid black;" href="javascript:cargarContenido_correos('createCorreos.php')">Redactar</a>
-    <button id="tres" style="border: 1px solid black;" onclick="readCorreosS()">Correos enviados</button>
+    <a id="tres" style="border: 1px solid black;" href="javascript:cargarContenido_correos('readCorreosSalidas.php')">Correos enviados</a>
 </div>
 <div id="bandeja">
 </div>
---
+++ parciales/final1/readCorreosSalidas.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+    include('conexion.php');
+    $sql = "select id, destino, asunto, mensaje, fecha from entradas";
+    $query=mysqli_query($conexion,$sql);
+?>
+<table border="1">
+    <tr>
+        <th>destino</th>
+        <th>Asunto</th>
+        <th>mensaje</th>
+        <th>fecha</th>
+    </tr>
+    
+    <?php
+        while ($fila = mysqli_fetch_array($query)){?>
+            <tr>
+                <td><?php echo $fila['destino'] ?></td>
+                <td><?php echo $fila['asunto'] ?></td>
+                <td><?php echo $fila['mensaje'] ?></td>
+                <td><?php echo $fila['fecha'] ?></td>
+            </tr>
+    <?php
+        }?>
+    </table>
+    <?php mysqli_close($conexion);?>
